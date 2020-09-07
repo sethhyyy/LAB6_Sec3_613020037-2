757334d5a13b0de4cd2b8d6df202d944d292bb55
seth-setthaphong <70099308+sethhyyy@users.noreply.github.com>
2020-09-07 16:31:11 +0700

    ส่งงาน Lab6 Sec3

---
+++ app/src/main/java/com/myweb/lab6menufragment/OneFragment.kt
@@ -0,0 +1,35 @@
+package com.myweb.lab6menufragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_one.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class OneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_one, container, false)
+            view.btnClickFragOne.setOnClickListener(){
+                var fragment: Fragment? = null
+                fragment = TwoFragment()
+                replaceFragment(fragment)
+            }
+            return view
+        // Inflate the layout for this fragment
+    }
+    fun replaceFragment(someFragment: Fragment){
+        val transaction = fragmentManager!!.beginTransaction()
+        transaction.replace(R.id.framelayout, someFragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
